14862ca55632ca3e8a8c6e70f84cab64f500dc36
masn19 <mas.null19@gmail.com>
2016-01-19 03:28:18 +0900

    add mid page

--- narou.rb
+++ narou.rb
@@ -7,13 +7,13 @@ def gen_ncode
   return "n#{[*0..9999].sample.to_s.rjust(4,'0')}#{[*"a".."zz"].sample}"
 end
 get '/' do
-  while true do
-    baseurl = "http://api.syosetu.com/novelapi/api/?out=json"
-    ncode = gen_ncode
-    res = Net::HTTP.get(URI.parse(baseurl + "&ncode=#{ncode}"))
-    data = JSON.parse(res)
-    if data[0]["allcount"].to_i != 0
-      redirect "http://ncode.syosetu.com/#{ncode}/"
-    end
+  baseurl = "http://api.syosetu.com/novelapi/api/?out=json"
+  ncodes = (1..100).map{gen_ncode}.join("-")
+  res = Net::HTTP.get(URI.parse(baseurl + "&ncode=#{ncodes}"))
+  @data = JSON.parse(res)
+  print @data
+  if @data[0]["allcount"].to_i == 0
+    redirect "/" 
   end
+  erb :index
 end
