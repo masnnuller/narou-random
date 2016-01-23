d44c9f4db832b0b84e39f101ab06869fdb6ae420
masn19 <mas.null19@gmail.com>
2016-01-24 02:19:41 +0900

    r-18を含む, 完結のみの選択肢を追加

--- narou.rb
+++ narou.rb
@@ -2,16 +2,45 @@ require 'sinatra'
 require 'json'
 require 'net/http'
 
-
-def gen_ncode
-  return "n#{[*0..9999].sample.to_s.rjust(4,'0')}#{[*"a".."zz"].sample}"
+def req(uri)
+  return JSON.parse(
+    Net::HTTP.get(uri)
+  )
 end
-get '/' do
+
+def get_data(fin,r18)
   baseurl = "http://api.syosetu.com/novelapi/api/?out=json"
-  ncodes = (1..100).map{gen_ncode}.join("-")
-  res = Net::HTTP.get(URI.parse(baseurl + "&ncode=#{ncodes}"))
-  @data = JSON.parse(res)
-  if @data[0]["allcount"].to_i == 0
+  r18_url = "http://api.syosetu.com/novel18api/api/?out=json"
+  params ||= ""
+  res ||= []
+
+  ncodes = (1..100).map{
+    "n#{[*0..9999].sample.to_s.rjust(4,'0')}#{[*"a".."zz"].sample}"
+  }.join("-")
+  if fin
+    params += "&stop=1"
+  end
+  res = req(URI.parse(baseurl + "&ncode=#{ncodes}" + params)).drop(1)
+  if r18
+    res += req(URI.parse(r18_url + "&ncode=#{ncodes}" + params)).drop(1)
+  end
+  return res.sample
+end
+
+get '/' do
+  @checked ||= {}
+  if params[:stop] == "true"
+    fin = true
+    @checked[:stop] = true
+  end
+  if params[:r18] == "true"
+    r18 = true
+    @checked[:r18] = true
+  end
+  fin ||= false
+  r18 ||= false
+  @data = get_data(fin=fin, r18=r18)
+  if @data.nil?
     redirect "/" 
   end
   erb :index
